2ef57729495583ae0fc7dde2993d10b5babb8ff2
CHENPrime-coder <chenbprime@qq.com>
2025-05-05 21:25:47 +0800

    feat: add PWA support with service worker and install badge
    - Created pnpm-workspace.yaml to manage built dependencies.
    - Added favicon.svg for PWA icon.
    - Configured pwa-assets.config.js for asset generation.
    - Implemented App.vue with demo button and PWABadge component.
    - Added vue.svg for Vue logo in assets.
    - Removed HelloWorld.vue component as it was empty.
    - Developed PWABadge.vue for PWA installation and update handling.
    - Set up main.js to initialize Vue application.
    - Styled application with Tailwind CSS in style.css.
    - Configured Vite with PWA plugin and Tailwind CSS in vite.config.js.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import PWABadge from './components/PWABadge.vue'
+
+function demo() {
+  console.log('demo')
+}
+</script>
+
+<template>
+  <input class="btn btn-primary" type="button" value="demo" @click="demo"/>
+  <br>
+  <br>
+  <PWABadge />
+</template>
---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: 'auto',
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'pwa-demo',
+      short_name: 'pwa-demo',
+      description: 'pwa-demo',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: true,  // 启用开发模式下的PWA功能
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  }), tailwindcss()],
+})
